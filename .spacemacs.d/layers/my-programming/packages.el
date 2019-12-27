a63da972731444e45709cb90d69942c7a60af881
oouxx <wangxinxing961129@gmail.com>
2019-12-27 18:39:07 +0800

    add many features

--- .spacemacs.d/layers/my-programming/packages.el
+++ .spacemacs.d/layers/my-programming/packages.el
@@ -29,34 +29,131 @@
 
 ;;; Code:
 
-(defconst my-programming-packages
-  '()
-  "The list of Lisp packages required by the my-programming layer.
+(setq my-programming-packages
+      '(
+        js2-mode
+        (cc-mode :location built-in)
+        lsp-java
+        company
+        company-lsp
+        company-tabnine
+        ))
 
-Each entry is either:
+(defun my-programming-packages/post-init-cc-mode ()
+  (use-package cc-mode
+    :config
+    (setq c-basic-offset 4)
+    ;; give me NO newline automatically after electric expressions are entered
+    (setq c-auto-newline nil)
 
-1. A symbol, which is interpreted as a package to be installed, or
+    ;; syntax-highlight aggressively
+    ;; (setq font-lock-support-mode 'lazy-lock-mode)
+    (setq lazy-lock-defer-contextually t)
+    (setq lazy-lock-defer-time 0)
+    ;make DEL take all previous whitespace with it
+    (c-toggle-hungry-state 1)
 
-2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
-    name of the package to be installed or loaded, and KEYS are
-    any number of keyword-value-pairs.
+    ;; indent
+    ;; google "C/C++/Java code indentation in Emacs" for more advanced skills
+    ;; C code:
+    ;;   if(1) // press ENTER here, zero means no indentation
+    (fix-c-indent-offset-according-to-syntax-context 'substatement 0)
+    ;;   void fn() // press ENTER here, zero means no indentation
+    (fix-c-indent-offset-according-to-syntax-context 'func-decl-cont 0)))
 
-    The following keys are accepted:
+(defun my-programming-packages/post-init-js2-mode ()
+  (use-package js2-mode)
+  )
 
-    - :excluded (t or nil): Prevent the package from being loaded
-      if value is non-nil
+(defun my-programming-packages/post-init-lsp-java ()
+  (use-package lsp-java
+    :after lsp-mode
+    :if *mvn*
+    :config
+    (use-package request :defer t)
+    :custom
+    (lsp-java-server-install-dir (expand-file-name "~/.lsp/eclipse.jdt.ls/"))))
 
-    - :location: Specify a custom installation location.
-      The following values are legal:
+(defun my-programming-packages/post-init-company ()
+  (use-package company
+    :diminish company-mode
+    :hook ((prog-mode LaTeX-mode latex-mode ess-r-mode) . company-mode)
+    :bind
+    (:map company-active-map
+          ([tab] . smarter-yas-expand-next-field-complete)
+          ("TAB" . smarter-yas-expand-next-field-complete))
+    :custom
+    (company-minimum-prefix-length 1)
+    (company-tooltip-align-annotations t)
+    (company-begin-commands '(self-insert-command))
+    (company-require-match 'never)
+    ;; Don't use company in the following modes
+    (company-global-modes '(not shell-mode eaf-mode))
+    ;; Trigger completion immediately.
+    (company-idle-delay 0.1)
+    ;; Number the candidates (use M-1, M-2 etc to select completions).
+    (company-show-numbers t)
+    :config
+    ;;(unless *clangd* (delete 'company-clang company-backends))
+    (global-company-mode 1)
+    (defun smarter-yas-expand-next-field-complete ()
+      "Try to `yas-expand' and `yas-next-field' at current cursor position.
+  If failed try to complete the common part with `company-complete-common'"
+      (interactive)
+      (if yas-minor-mode
+          (let ((old-point (point))
+                (old-tick (buffer-chars-modified-tick)))
+            (yas-expand)
+            (when (and (eq old-point (point))
+                      (eq old-tick (buffer-chars-modified-tick)))
+              (ignore-errors (yas-next-field))
+              (when (and (eq old-point (point))
+                        (eq old-tick (buffer-chars-modified-tick)))
+                (company-complete-common))))
+        (company-complete-common))))
+    )
 
-      - The symbol `elpa' (default) means PACKAGE will be
-        installed using the Emacs package manager.
 
-      - The symbol `local' directs Spacemacs to load the file at
-        `./local/PACKAGE/PACKAGE.el'
+(defun my-programming-packages/post-init-company-lsp ()
+  (use-package company-lsp
+    :defer t
+    :custom (company-lsp-cache-candidates 'auto)))
 
-      - A list beginning with the symbol `recipe' is a melpa
-        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
+(defun my-programming-packages/post-init-company-tabnine()
+  (use-package company-tabnine
+    :defer 1
+    :custom
+    (company-tabnine-max-num-results 9)
+    :bind
+    (("M-q" . company-other-backend)
+    ("C-z t" . company-tabnine))
+    :hook
+    (lsp-after-open . (lambda ()
+                        (setq company-tabnine-max-num-results 3)
+                        (add-to-list 'company-transformers 'company//sort-by-tabnine t)
+                        (add-to-list 'company-backends '(company-lsp :with company-tabnine :separate))))
+    :config
+    ;; Enable TabNine on default
+    (add-to-list 'company-backends #'company-tabnine)
 
-
+    ;; Integrate company-tabnine with lsp-mode
+    (defun company//sort-by-tabnine (candidates)
+      (if (or (functionp company-backend)
+              (not (and (listp company-backend) (memq 'company-tabnine company-backend))))
+          candidates
+        (let ((candidates-table (make-hash-table :test #'equal))
+              candidates-lsp
+              candidates-tabnine)
+          (dolist (candidate candidates)
+            (if (eq (get-text-property 0 'company-backend candidate)
+                    'company-tabnine)
+                (unless (gethash candidate candidates-table)
+                  (push candidate candidates-tabnine))
+              (push candidate candidates-lsp)
+              (puthash candidate t candidates-table)))
+          (setq candidates-lsp (nreverse candidates-lsp))
+          (setq candidates-tabnine (nreverse candidates-tabnine))
+          (nconc (seq-take candidates-tabnine 3)
+                (seq-take candidates-lsp 6))))))
+)
 ;;; packages.el ends here
