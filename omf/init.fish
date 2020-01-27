4795e6f7bf00c3eb153971c4e857207b62f9bd1a
oouxx <wangxinxing961129@gmail.com>
2020-01-27 20:30:25 +0800

    try polybar-theme(polybar-8)

--- omf/init.fish
+++ omf/init.fish
@@ -27,17 +27,8 @@ alias ht="htop"
 alias vim="nvim"
 
 # command
-command ibus-daemon -d -x
+command ibus-daemon -d -x -r
 command setxkbmap -option ctrl:nocaps
 
 # disable greeting message
 set fish_greeting
-
-# highlight
-set hilite (which highlight)
-set -xg LESSOPEN "| $hilite %s --out-format xterm256 --quiet --force "
-set -xg LESS " -R"
-
-function less
-    command less -m -N -g -i -J --underline-special --SILENT $argv
-end
